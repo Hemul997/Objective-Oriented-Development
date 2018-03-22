5ad8c65aa9299305ca9572c0cbfcfd44ad0c1a4a
Hemul997 <nikitamoshkin1997@gmail.com>
2018-03-22 16:26:52 +0300

    Добавил основной код программы

--- BigNumber/src/main/java/volgatech/ood2018/Main.java
+++ BigNumber/src/main/java/volgatech/ood2018/Main.java
@@ -1,22 +1,31 @@
 package volgatech.ood2018;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) throws Exception{
-//        String firstOperand = "55555551";
-//        String secondOperand = "55555555";
-        //String secondOperand = "22";
-        //String firstOperand = "-10101010101010101010101010101010101010";
-        String firstOperand = "50000000001";
-        String secondOperand = "15555555551";
-        BigNumber firstNumber = new BigNumber(firstOperand);
-        BigNumber secondNumber = new BigNumber(secondOperand);
-        BigNumber sumNumbers = firstNumber.subtract(secondNumber);
-        firstNumber.printValue();
-        System.out.println();
-        secondNumber.printValue();
-        System.out.println();
-        sumNumbers.printValue();
-//        System.out.println(bigNumber.toString());
-//        System.out.println(sumNumbers.toString());
+    public static void main(String[] args) {
+        List<String> arguments;
+        String inpFile = args[0];
+        String outFile = args[1];
+
+        Path path = Paths.get(inpFile);
+        try {
+            arguments = Files.readAllLines(path);
+            FileWriter writer = new FileWriter(outFile);
+            BigNumberController bigNumberController = new BigNumberController();
+            for (String line : arguments) {
+                bigNumberController.setParams(line);
+                writer.write(line + " = " + bigNumberController.processResult() + '\n');
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
