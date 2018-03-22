8b7f87d097f3ae5187e5b0af37f6d3dcfc430c45
Hemul997 <nikitamoshkin1997@gmail.com>
2018-03-22 16:22:22 +0300

    Добавил операцию деления в BigNumber. Исправил косяк в вычитании, из за которого деление работало неправильно.

--- BigNumber/src/main/java/volgatech/ood2018/BigNumber.java
+++ BigNumber/src/main/java/volgatech/ood2018/BigNumber.java
@@ -1,257 +1,237 @@
 package volgatech.ood2018;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 
-import static java.lang.Boolean.FALSE;
-import static java.lang.Boolean.TRUE;
-
 public class BigNumber {
-    final static int BIG_NUMBER_BASE = 10;
-    final static char POSITIVE_NUMBER_SIGH = '+';
-    final static char NEGATIVE_NUMBER_SIGH = '-';
 
-    private List<Character> number;
-    private char sign;
+    private final static int BIG_NUMBER_BASE = 10;
 
-    public List<Character> getNumber() {
-        return number;
+    private List<Character> value = new ArrayList<>();
+
+    public BigNumber() {
+
     }
 
-    public void setNumber(List<Character> number) {
-        this.number = number;
+    public BigNumber(List<Character> value) {
+        this.value = new ArrayList<>(value);
     }
 
-    public void setNumber(String strNumber) {
-        this.number = strNumber.chars()
+    public BigNumber(String value) {
+        this.value = value.chars()
                 .mapToObj(e->(char)e)
                 .collect(Collectors.toList());
-    }
-
-    BigNumber(String strNumber){
-        if (strNumber.charAt(0) == '-') {
-            this.number = strNumber.substring(1, strNumber.length())
-                    .chars()
-                    .mapToObj(e->(char)e)
-                    .collect(Collectors.toList());
-            this.sign = NEGATIVE_NUMBER_SIGH;
-        } else {
-            this.number = strNumber.chars()
-                    .mapToObj(e->(char)e)
-                    .collect(Collectors.toList());
-            this.sign = POSITIVE_NUMBER_SIGH;
-        }
-
-    }
-
-    BigNumber() {
-        this.number = new ArrayList<>();
-        this.sign = POSITIVE_NUMBER_SIGH;
-    }
-
-
-    public BigNumber add(BigNumber value) {
-        BigNumber returnNumber = new BigNumber();
-        List<Character> thisNumber = new ArrayList<>(this.getNumber());
-        List<Character> otherNumber = new ArrayList<>(value.getNumber());
-        Collections.reverse(thisNumber);
-        Collections.reverse(otherNumber);
-        List<Character> returnValue = new ArrayList<>();
-        int number;
-        int firstOperand, secondOperand;
-        Boolean isDigit = FALSE;
-        int maxSize = Math.max(thisNumber.size(), otherNumber.size()) + 1;
-        for (int i = 0; i < maxSize; ++i) {
-            firstOperand = getNumberValue(thisNumber, i);
-            secondOperand = getNumberValue(otherNumber, i);
-            number = firstOperand + secondOperand;
-            if (isDigit) {
-                isDigit = FALSE;
-                ++number;
-            }
-            if (nextDigit(number)) {
-                isDigit = TRUE;
-                number -= BIG_NUMBER_BASE;
-            }
-            returnValue.add(Character.forDigit(number, BIG_NUMBER_BASE));
-        }
-        if (returnValue.get(maxSize - 1) == '0') {
-            returnValue.remove(maxSize- 1);
-        }
-        Collections.reverse(returnValue);
-        returnNumber.setNumber(returnValue);
-        return returnNumber;
-    }
-
-    public char getSign() {
-        return sign;
-    }
-    
-    public int getCountDigits() {
-        return this.number.size();
-    }
-
-    public void setSign(char sign) {
-        if (sign == NEGATIVE_NUMBER_SIGH) {
-            this.sign = NEGATIVE_NUMBER_SIGH;
-        } else {
-            this.sign = POSITIVE_NUMBER_SIGH;
-        }
-
-    }
-
-    public BigNumber subtract(BigNumber value) {
-        int number;
-        int firstOperand, secondOperand;
-        BigNumber returnNumber = new BigNumber();
-        List<Character> subtractiveNumber = new ArrayList<>();
-        List<Character> subtrahendNumber = new ArrayList<>();
-        List<Character> returnValue = new ArrayList<>();
-        switch (findMaxNumber(this.getNumber(), value.getNumber())) {
-            case 0:
-                returnValue.add('0');
-                break;
-            case 1:
-                subtractiveNumber.addAll(this.getNumber());
-                subtrahendNumber.addAll(value.getNumber());
-                break;
-            case -1:
-                subtractiveNumber.addAll(value.getNumber());
-                subtrahendNumber.addAll(this.getNumber());
-                returnNumber.setSign(NEGATIVE_NUMBER_SIGH);
-                break;
-        }
-        if (returnValue.isEmpty()) {
-            Collections.reverse(subtractiveNumber);
-            Collections.reverse(subtrahendNumber);
-
-            Boolean getDigit = FALSE;
-            for (int i = 0; i <= subtrahendNumber.size(); ++i) {
-                if ((i == subtrahendNumber.size() && !getDigit) || (i == subtractiveNumber.size())) {
-                    break;//fix bug with 105 - 6
-                }
-
-                firstOperand = getNumberValue(subtractiveNumber, i);
-                secondOperand = getNumberValue(subtrahendNumber, i);
-                if (getDigit) {
-                    --firstOperand;
-                }
-                if (firstOperand < secondOperand) {
-                    firstOperand += BIG_NUMBER_BASE;
-                    getDigit = TRUE;
-                }
-                number = firstOperand - secondOperand;
-                returnValue.add(Character.forDigit(number, BIG_NUMBER_BASE));
-            }
-            Collections.reverse(returnValue);
-            deleteRedundantZeros(returnValue);
-        }
-        returnNumber.setNumber(returnValue);
-        return returnNumber;
-    }
-
-    public BigNumber multiply(BigNumber value) {
-        BigNumber returnNumber = new BigNumber();
-        if (this.isNegative() || value.isNegative()) {
-            returnNumber.setSign(NEGATIVE_NUMBER_SIGH);
-        }
-        List<Character> thisNumber = new ArrayList<>(this.getNumber());
-        List<Character> otherNumber = new ArrayList<>(value.getNumber());
-        Collections.reverse(thisNumber);
-        Collections.reverse(otherNumber);
-        List<Character> returnValue = new ArrayList<>();
-        int firstOperand, secondOperand;
-        char[] number = new char[thisNumber.size() + otherNumber.size()];
-        int length = thisNumber.size() + otherNumber.size();
-        for (int i = 0; i < thisNumber.size(); ++i) {
-            for (int j = 0; j < otherNumber.size() ; ++j) {
-                firstOperand = getNumberValue(thisNumber, i);
-                secondOperand = getNumberValue(otherNumber, j);
-                number[i + j] += firstOperand * secondOperand;
-            }
-        }
-        for (int i = 0; i < length - 1; ++i) {
-            number[i + 1] += number[i] / BIG_NUMBER_BASE;
-            number[i] %= BIG_NUMBER_BASE;
-        }
-        for (char num : number) {
-            returnValue.add(Character.forDigit(num, BIG_NUMBER_BASE));
-        }
-        Collections.reverse(returnValue);
-        deleteRedundantZeros(returnValue);
-        returnNumber.setNumber(returnValue);
-        return returnNumber;
-    }
-
-//    //todo
-//    public BigNumber divide(BigNumber value) throws Exception {
-//
-//        if (value.getNumber().get(0) == '0' && value.getCountDigits() == 1) {
-//            throw new Exception("На ноль делить нельзя!");
-//        }
-//
-//        if (this.getCountDigits() < value.getCountDigits()) {
-//            return new BigNumber("0");
-//        }
-//
-//
-//    }
-
-    @Override
-    public String toString() {
-        return "BigNumber{" +
-                "number=" + number.toString() +
-                '}';
-    }
-
-    public void printValue() {
-        if (isNegative())
-            System.out.print(this.sign);
-        this.number.forEach(System.out::print);
-    }
-
-    private boolean nextDigit(int number) {
-        return (number >= BIG_NUMBER_BASE);
+        Collections.reverse(this.value);
+        deleteRedundantZeros();
     }
 
     private int getNumberValue(List<Character> thisNumber, int index) {
         return thisNumber.size() <= index ? 0 : Character.getNumericValue(thisNumber.get(index));
     }
 
-    private int findMaxNumber(List<Character> firstNumber, List<Character> secondNumber) {
-        int result = 0;
-        if (secondNumber.size() < firstNumber.size()) {
-            result = 1;
-        } else {
-            if (secondNumber.size() > firstNumber.size())
-                result = -1;
-            else
-                for (int i = 0; i < firstNumber.size(); ++i) {
-                    if (firstNumber.get(i) > secondNumber.get(i)) {
-                        result = 1;
-                        break;
-                    }
-                    if (firstNumber.get(i) < secondNumber.get(i)) {
-                        result= -1;
-                        break;
-                    }
-                }
+    private boolean nextDigit(int number) {
+        return (number >= BIG_NUMBER_BASE);
+    }
+
+    public BigNumber add(BigNumber addNumber) {
+        BigNumber returnNumber = new BigNumber();
+        List<Character> returnValue = new ArrayList<>();
+
+        int additionResult;
+        int firstOperand, secondOperand;
+        Boolean isDigit = false;
+        int maxSize = Math.max(this.size(), addNumber.size()) + 1;
+        for (int i = 0; i < maxSize; ++i) {
+            firstOperand = getNumberValue(this.value, i);
+            secondOperand = getNumberValue(addNumber.value, i);
+            additionResult = firstOperand + secondOperand;
+            if (isDigit) {
+                isDigit = false;
+                ++additionResult;
+            }
+            if (nextDigit(additionResult)) {
+                isDigit = true;
+                additionResult -= BIG_NUMBER_BASE;
+            }
+            returnValue.add(Character.forDigit(additionResult, BIG_NUMBER_BASE));
         }
-        return result;
+        if (returnValue.get(maxSize - 1) == '0') {
+            returnValue.remove(maxSize - 1);
+        }
+
+        returnNumber.setNumber(returnValue);
+        return returnNumber;
     }
 
-    private boolean isNegative() {
-        return (this.sign == NEGATIVE_NUMBER_SIGH);
+    public void setNumber(List<Character> number) {
+        this.value.addAll(number);
     }
 
-    private void deleteRedundantZeros(List<Character> number) {
-        for (int i = 0 ; i < number.size(); ++i) {
-            if (!number.get(i).equals('0')) {
-                break;
-            } else {
-                number.remove(i);
-                --i;
+    public BigNumber subtract(BigNumber subNumber) {
+        List<Character> returnValue = new ArrayList<>();
+
+        boolean isLastDigit = false;
+        for (int i = 0; i < subNumber.size(); i++) {
+            int firstOperand = getNumberValue(this.value, i);
+            int secondOperand = getNumberValue(subNumber.value, i);
+            int subResult = firstOperand - secondOperand;
+
+            if (isLastDigit) {
+                subResult--;
+                isLastDigit = false;
+            }
+
+            if (subResult < 0) {
+                isLastDigit = true;
+                subResult += BIG_NUMBER_BASE;
+            }
+            returnValue.add(Character.forDigit(subResult, BIG_NUMBER_BASE));
+        }
+
+        if (isLastDigit) {
+            returnValue.add('0');
+        }
+
+        BigNumber returnNumber = new BigNumber(returnValue);
+        returnNumber.deleteRedundantZeros();
+        return returnNumber;
+
+    }
+
+    public BigNumber multiply(BigNumber number) {
+        BigNumber returnNumber = new BigNumber();
+        List<Character> thisNumber = new ArrayList<>(this.value);
+        List<Character> otherNumber = new ArrayList<>(number.value);
+        List<Character> returnValue = new ArrayList<>();
+
+        int firstOperand, secondOperand;
+        char[] num = new char[thisNumber.size() + otherNumber.size()];
+        int length = thisNumber.size() + otherNumber.size();
+        for (int i = 0; i < thisNumber.size(); ++i) {
+            for (int j = 0; j < otherNumber.size() ; ++j) {
+                firstOperand = getNumberValue(thisNumber, i);
+                secondOperand = getNumberValue(otherNumber, j);
+                num[i + j] += firstOperand * secondOperand;
             }
         }
+        for (int i = 0; i < length - 1; ++i) {
+            num[i + 1] += num[i] / BIG_NUMBER_BASE;
+            num[i] %= BIG_NUMBER_BASE;
+        }
+        for (char ch : num) {
+            returnValue.add(Character.forDigit(ch, BIG_NUMBER_BASE));
+        }
+
+        returnNumber.setNumber(returnValue);
+        returnNumber.deleteRedundantZeros();
+        return returnNumber;
+    }
+
+    public BigNumber divide(BigNumber number) throws Exception {
+        if (number.compareTo(new BigNumber("0")) == 0) {
+            throw new Exception("Division by ZERO!!!");
+        }
+        List<Character> returnValue = new ArrayList<>();
+        BigNumber thisNumber = new BigNumber(new ArrayList<>(this.value));
+
+        if (this.size() < number.size()) {
+            return new BigNumber("0");
+        }
+
+        int digitCount = 0;
+        while (!thisNumber.value.isEmpty()) {
+            BigNumber dividend = createNumberForDivision(thisNumber, number, returnValue, digitCount);
+            digitCount = 0;
+            BigNumber multiplicationResult = new BigNumber("0");
+            for (int divCoefficient = BIG_NUMBER_BASE - 1; divCoefficient >= 0; divCoefficient--) {
+                multiplicationResult = number.multiply(new BigNumber(Integer.toString(divCoefficient)));
+                if (multiplicationResult.compareTo(dividend) != 1) {
+                    returnValue.add(Character.forDigit(divCoefficient, BIG_NUMBER_BASE));
+                    break;
+                }
+            }
+            BigNumber iterRes = dividend.subtract(multiplicationResult);
+
+            if (iterRes.compareTo(new BigNumber("0")) != 0 && !thisNumber.value.isEmpty()) {
+                digitCount = iterRes.size();
+                for (int i = 0; i < iterRes.size(); i++) {
+                    thisNumber.value.add(iterRes.value.get(i));
+                }
+            }
+        }
+
+        Collections.reverse(returnValue);
+        BigNumber returnNumber = new BigNumber(returnValue);
+        returnNumber.deleteRedundantZeros();
+        return returnNumber;
+    }
+
+    private BigNumber createNumberForDivision(BigNumber mainDividend, BigNumber divider,
+                                      List<Character> newNumberData, int digitCount) {
+        String valueForDiv = "";
+        for (int i = 0; i <= digitCount && mainDividend.size() != 0; i++) {
+            valueForDiv += mainDividend.value.get(mainDividend.size() - 1).toString();
+            mainDividend.value.remove(mainDividend.size() - 1);
+        }
+
+        BigNumber dividend = new BigNumber(valueForDiv);
+        do {
+            if (dividend.compareTo(divider) >= 0 || mainDividend.value.isEmpty()) {
+                break;
+            }
+            if (digitCount >= 0) {
+                newNumberData.add('0');
+            }
+            int lastElement = mainDividend.size() - 1;
+            dividend.value.add(0, mainDividend.value.get(lastElement));
+            mainDividend.value.remove(lastElement);
+        } while (!mainDividend.value.isEmpty());
+
+        return dividend;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder value = new StringBuilder();
+        for (int i = this.value.size() - 1; i >= 0; i--) {
+            value.append(this.value.get(i));
+        }
+        return value.toString();
+    }
+
+    private void deleteRedundantZeros() {
+        for (int i = this.size() - 1; this.size() > 1 && i >= 0; i--) {
+            if (value.get(i) != '0') {
+                break;
+            } else {
+                value.remove(i);
+            }
+        }
+    }
+
+    public int size() {
+        return value.size();
+    }
+
+    public int compareTo(BigNumber number) {
+        int result = 0;
+        if (this.size() > number.size()) {
+            result = 1;
+        } else if (this.size() < number.size()) {
+            result = -1;
+        } else {
+            for (int i = number.size() - 1; i >= 0; i--) {
+                if (this.value.get(i) > number.value.get(i)) {
+                    result = 1;
+                    break;
+                } else if (this.value.get(i) < number.value.get(i)) {
+                    result = -1;
+                    break;
+                }
+            }
+        }
+        return result;
     }
 }
